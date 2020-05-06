2fb2fdcf7e5d50073f67fa88a008267858f4b9e6
Matthew Beck <matthew.beck@digital.hmrc.gov.uk>
2020-05-06 13:52:46 +0100

    CTCTRADERS-445: Removed UI and updated logging

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,15 +6,8 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "org.reactivemongo" %% "play2-reactivemongo"           % "0.18.6-play26",
-    "uk.gov.hmrc"       %% "logback-json-logger"           % "4.6.0",
-    "uk.gov.hmrc"       %% "play-health"                   % "3.14.0-play-26",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.2.0-play-26",
-    "uk.gov.hmrc"       %% "bootstrap-play-26"             % "1.7.0",
-    "uk.gov.hmrc"       %% "play-whitelist-filter"         % "3.1.0-play-26",
-    "uk.gov.hmrc"       %% "play-nunjucks"                 % "0.23.0-play-26",
-    "uk.gov.hmrc"       %% "play-nunjucks-viewmodel"       % "0.8.0-play-26",
-    "org.webjars.npm"   % "govuk-frontend"                 % "3.3.0"
+    "uk.gov.hmrc"       %% "bootstrap-play-26"             % "1.4.0",
+    "uk.gov.hmrc"       %% "bootstrap-play-26"             % "1.7.0"
   )
 
   val test = Seq(
@@ -22,6 +15,6 @@ object AppDependencies {
     "com.typesafe.play"       %% "play-test"                % current                 % "test",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test",
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % "test",
-    "org.mockito"             %  "mockito-all"              % "1.10.19",
+    "org.mockito"             %  "mockito-all"              % "1.10.19"
   )
 }
