d12fce3ba68f9cd90451cf619d47e09c73ec3dcf
Kevin <29205960+kevincoatsworth@users.noreply.github.com>
2020-07-10 13:03:10 +0100

    CTCTRADERS-543: Added MRN generator without page styling.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,19 +1,24 @@
-import play.core.PlayVersion.current
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
+import play.core.PlayVersion
 import sbt._
 
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-play-26"             % "1.7.0"
+    "uk.gov.hmrc"       %% "bootstrap-play-26"             % "1.7.0",
+    "uk.gov.hmrc"       %% "play-nunjucks"                 % "0.23.0-play-26",
+    "uk.gov.hmrc"       %% "play-nunjucks-viewmodel"       % "0.8.0-play-26",
+    "org.webjars.npm"   % "govuk-frontend"                 % "3.3.0",
+    "wolfendale"        %% "scalacheck-gen-regexp"         % "0.1.1"
   )
 
   val test = Seq(
-    "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test",
-    "com.typesafe.play"       %% "play-test"                % current                 % "test",
-    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % "test",
-    "org.mockito"             %  "mockito-all"              % "1.10.19"
+    "org.scalatest"               %% "scalatest"          % "3.0.7",
+    "org.scalatestplus.play"      %% "scalatestplus-play" % "3.1.2",
+    "org.pegdown"                 %  "pegdown"            % "1.6.0",
+    "org.jsoup"                   %  "jsoup"              % "1.10.3",
+    "com.typesafe.play"           %% "play-test"          % PlayVersion.current,
+    "org.mockito"                 %  "mockito-all"        % "1.10.19",
+    "org.scalacheck"              %% "scalacheck"         % "1.14.0",
+    "com.github.tomakehurst"      % "wiremock-standalone" % "2.25.0"
   )
 }
