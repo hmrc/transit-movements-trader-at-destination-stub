d48eb3421d569f8ff0ad781b870b897bd30b4647
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2021-05-05 17:41:13 +0100

    Revert "CTCTRADRES-2244:update app conf"
    This reverts commit 63fb0283

--- app/connectors/DestinationConnector.scala
+++ app/connectors/DestinationConnector.scala
@@ -17,12 +17,10 @@
 package connectors
 
 import config.AppConfig
-
 import javax.inject.Inject
 import uk.gov.hmrc.http.HeaderCarrier
-import uk.gov.hmrc.http.HttpClient
 import uk.gov.hmrc.http.HttpResponse
-
+import uk.gov.hmrc.play.bootstrap.http.HttpClient
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 import scala.xml.Node
