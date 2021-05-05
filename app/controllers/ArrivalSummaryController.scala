b83d9d3cd53466497124d3379a4ecce11c66a8a7
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2021-05-05 17:41:26 +0100

    Revert "CTCTRADRES-2244:format"
    This reverts commit aa817e3c

--- app/controllers/ArrivalSummaryController.scala
+++ app/controllers/ArrivalSummaryController.scala
@@ -18,9 +18,7 @@ package controllers
 
 import com.google.inject.Inject
 import play.api.Logger
-import play.api.mvc.Action
-import play.api.mvc.AnyContent
-import play.api.mvc.ControllerComponents
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 import utils.JsonUtils
 
