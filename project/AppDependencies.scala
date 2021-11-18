967006563c52b449f32dbb639073899c46750387
carrielies <carrielies@users.noreply.github.com>
2021-11-18 13:39:14 +0000

    CTCTRADERS-2785 Update more dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,12 +18,12 @@ object AppDependencies {
     "org.mockito"            % "mockito-core"              % "3.12.4",
     "org.scalatest"          %% "scalatest"                % "3.2.9",
     "com.typesafe.play"      %% "play-test"                % current,
-    "org.jsoup"              %  "jsoup"                    % "1.14.2",
+    "org.jsoup"              % "jsoup"                     % "1.14.2",
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0",
     "org.scalatestplus"      %% "mockito-3-2"              % "3.1.2.0",
-    "org.scalacheck"         %% "scalacheck"               % "1.15.4",
+    "org.scalatestplus"      %% "scalacheck-1-15"          % "3.2.9.0",
     "org.scalatestplus"      %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "com.github.tomakehurst" % "wiremock-standalone"       % "2.27.2",
-    "com.vladsch.flexmark"   % "flexmark-all"              % "0.35.10"
+    "com.vladsch.flexmark"   % "flexmark-all"              % "0.36.8"
   ).map(_ % "test")
 }
